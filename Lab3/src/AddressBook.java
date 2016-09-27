7b1f2de0cff284fc909e0e989f13940ee39d2a42
CyrusSadeghiEmamchai <CyrusSadeghiEmamchai@AA0508-21.labs.sce.carleton.ca>
2016-09-27 11:56:31 -0400

    First commit with files

---
+++ Lab3/src/AddressBook.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class AddressBook {
+	private List<BuddyInfo> buddies;
+	
+	public AddressBook()
+	{
+		buddies = new ArrayList<BuddyInfo>();
+	}
+	
+	public void addBuddy(BuddyInfo buddy)
+	{
+		if (buddy != null)
+		{
+			buddies.add(buddy);
+		}
+	}
+	
+	public boolean removeBuddy(BuddyInfo buddy)
+	{
+		return buddies.remove(buddy);
+	}
+	
+	public static void main(String[] args)
+	{
+		BuddyInfo buddy = new BuddyInfo("Cyrus", "Carleton", "613");
+		AddressBook book = new AddressBook();
+		
+		book.addBuddy(buddy);
+		book.removeBuddy(buddy);
+	}
+}
